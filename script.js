4efddcb0985971781eb488c49f814d6a15da3f2e
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-02-28 20:03:19 +0530

    signIn function

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ let database = {
     username: "shubham",
     password: "secretpass"
 }
-let newsFeed = {
-    username: "Shubham Patil",
-    Timeline: "Super excited to be a developer."
+let newsFeed = [
+    { 
+        username: "Shubham Patil", 
+        timeline: "Super excited to be a developer."
+    },
+    {
+        username: "Mahesh S",
+        timeline: "Hey there just cool wether outside!!"
+    }
+]
+let userPrompt = prompt("What's your username?")
+let passPrompt = prompt("What's your password?")
 
+function signIn(user, pass){
+    if (user === database.username && pass === database.password){
+        console.log(newsFeed);
+    } else {
+        alert("You entered wrong username")
+    }
 }
