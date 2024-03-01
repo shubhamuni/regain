07847b07e2f08fa5e59dc03822e83a450ab85f7e
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-01 18:58:38 +0530

    user validation

--- script.js
+++ script.js
@@ -34,14 +34,13 @@ let userPrompt = prompt("What's your username?")
 let passPrompt = prompt("What's your password?")
 
 function signIn(user, pass){
-    if (user === database[0].username && pass === database[0].password){
-        console.log(newsFeed)
-        alert(`Welcome to your profile ${database[0].username}`)
-    } else if (user === database[1].username && pass === database[1].password){
-        console.log(newsFeed)
-        alert(`Welcome to your profile ${database[1].username}`)
-    } else {
-        alert("You entered wrong username or password")
+    for (let i=0; i < database.length; i++){
+        if (user === database[i].username && pass === database[i].password){
+            console.log(newsFeed)
+            alert(`Welcome to your profile ${database[i].username}`)
+        }  else {
+            alert("You entered wrong username or password")
+        }
     }
 }
 signIn(userPrompt, passPrompt);
