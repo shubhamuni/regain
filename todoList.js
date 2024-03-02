1d1da2c47da60ce70d6157aa39ee06fa8b063271
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-02 19:08:23 +0530

    if statement

--- todoList.js
+++ todoList.js
@@ -2,9 +2,12 @@ let button = document.getElementById("enter")
 let input = document.getElementById("userinput")
 let ul = document.querySelector("ul");
 let appendList = function() {
-    let li = document.createElement("li");
+    if (input.value.length > 0){
+        let li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
 }
+    }
+    
 
 button.addEventListener("click",appendList)
