76d3dcb15c3898989cb086c1defa875138510127
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-02 19:06:19 +0530

    Merge into todos

--- todoList.js
+++ todoList.js
@@ -1,2 +1,10 @@
-let button = document.getElementById("button")
+let button = document.getElementById("enter")
 let input = document.getElementById("userinput")
+let ul = document.querySelector("ul");
+let appendList = function() {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+}
+
+button.addEventListener("click",appendList)
