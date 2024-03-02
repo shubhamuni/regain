41574d0e56bbf801156ded49dd0513e2cb76b2bd
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-02 19:40:21 +0530

    PWA

--- todoList.js
+++ todoList.js
@@ -2,16 +2,25 @@ let button = document.getElementById("enter")
 let input = document.getElementById("userinput")
 let ul = document.querySelector("ul");
 
-let appendList = function(event) {
-    // console.log(event)
-    if (input.value.length > 0 && event.key === "Enter"){
-        let li = document.createElement("li");
+function createListItem() {
+    let li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value ="";
+}
+
+let addListAfterClick = function() {
+    if (input.value.length > 0){
+        createListItem();
+    }
+}
+let addListAfterEnter = function(event) {
+    // console.log(event)
+    if (input.value.length > 0 && event.key === "Enter"){
+        createListItem();
     }
 }
     
 
-button.addEventListener("click",appendList)
-input.addEventListener("keydown",appendList)
+button.addEventListener("click",addListAfterClick)
+input.addEventListener("keydown",addListAfterEnter)
