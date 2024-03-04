9962f1bba4262b48248217f5933ad26013d2f8ec
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-04 08:29:55 +0530

    CSS checked

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
 let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1")
 let color2 = document.querySelector(".color2")
+console.log(css);
+console.log(color1);
+console.log(color2);
