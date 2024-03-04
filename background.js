0655bff32481c63062a91453f73693232048ad19
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-04 08:44:10 +0530

    color gradient by selection

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
 let css = document.querySelector("h3");
-let color1 = document.querySelector(".color1")
-let color2 = document.querySelector(".color2")
-console.log(css);
-console.log(color1);
-console.log(color2);
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
+
+color1.addEventListener("input", function() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + " )";
+})
+color2.addEventListener("input", function() {
+    body.style.background = "linear-gradient(to right, " + color2.value + ", " + color1.value + " )";
+})
