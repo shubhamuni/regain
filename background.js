34285bff9f369673cc78da6219cc98cf76893027
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-04 19:36:13 +0530

    gradient function

--- background.js
+++ background.js
@@ -2,11 +2,8 @@ let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
-
-
-color1.addEventListener("input", function() {
+let changeColor = function() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + " )";
-})
-color2.addEventListener("input", function() {
-    body.style.background = "linear-gradient(to right, " + color2.value + ", " + color1.value + " )";
-})
+};
+color1.addEventListener("input",changeColor);
+color2.addEventListener("input",changeColor);
