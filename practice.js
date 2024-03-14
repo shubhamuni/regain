650fb257cbb1176c2cc34545edc03074240f7a51
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-14 07:24:07 +0530

    area calc

--- practice.js
+++ practice.js
@@ -1,15 +1,33 @@
-console.log("Start");
+const radius = [3,2,1,4];
 
-function cb() {
-    console.log("Callback called")
+function calculateArea(x) {
+    const output = [];
+    for(let i = 0; i < radius.length; i++){
+        output.push(Math.PI * x[i]* x[i])
+    }
+    return output;
 }
-setTimeout(cb, 1000);
+console.log(calculateArea(radius))
 
-console.log("End calling")
 
-let start = new Date().getTime();
-let end = start;
 
-while(end < start + 7000){
-    end = new Date().getTime();
-}
+
+
+
+
+
+// console.log("Start");
+
+// function cb() {
+//     console.log("Callback called")
+// }
+// setTimeout(cb, 1000);
+
+// console.log("End calling")
+
+// let start = new Date().getTime();
+// let end = start;
+
+// while(end < start + 7000){
+//     end = new Date().getTime();
+// }
