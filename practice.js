bf6b0b1285a8745f50bd04905dc6e7c81a0002af
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-14 19:57:54 +0530

    add array of objects

--- practice.js
+++ practice.js
@@ -1,35 +1,44 @@
-const array = [2,1,4,3,7,8,9,5];
+//Examples of map, filter and reduce
+const users = [
+    {firstName:"Shubham",lastName:"Patil",age:25},
+    {firstName:"Akshay",lastName:"Kumar",age:58},
+    {firstName:"Akshay",lastName:"Saini",age:25},
+    {firstName:"Elon",lastName:"Musk",age:54},
+    {firstName:"Sunil",lastName:"Shetty",age:35},
+]
 
-function isOdd(x) {
-    return x % 2;
-}
+// const array = [2,1,4,3,7,8,9,5];
 
-const out = array.filter(isOdd)
-console.log(out)
+// function isOdd(x) {
+//     return x % 2;
+// }
+
+// const out = array.filter(isOdd)
+// console.log(out)
 
 
-const radius = [3,2,1,4];
+// const radius = [3,2,1,4];
 
-const area = function (radius) {
-    return Math.PI*radius*radius;
-};
+// const area = function (radius) {
+//     return Math.PI*radius*radius;
+// };
 
-const circumference = function (radius) {
-    return 2*Math.PI*radius;
-}
-const diameter = function (radius){
-    return 2*radius;
-}
+// const circumference = function (radius) {
+//     return 2*Math.PI*radius;
+// }
+// const diameter = function (radius){
+//     return 2*radius;
+// }
 
-//This below function is also converted as same as map method
+// //This below function is also converted as same as map method
 
-Array.prototype.calculate = function (logic) {
-    const output = [];
-    for(let i = 0; i < this.length; i++){
-        output.push(logic(this[i]))
-    }
-    return output;
-};
+// Array.prototype.calculate = function (logic) {
+//     const output = [];
+//     for(let i = 0; i < this.length; i++){
+//         output.push(logic(this[i]))
+//     }
+//     return output;
+// };
 
 // This below function is also converted as same as map method
 // function calculateArea(x,logic) {
@@ -41,11 +50,11 @@ Array.prototype.calculate = function (logic) {
 // };
 
 // area of a circle is also can be calculated by array.map method as below
-console.log(radius.map(area));
+// console.log(radius.map(area));
 
-console.log(radius.calculate(area));
-console.log(radius.calculate(circumference));
-console.log(radius.calculate(diameter));
+// console.log(radius.calculate(area));
+// console.log(radius.calculate(circumference));
+// console.log(radius.calculate(diameter));
 
 
 
