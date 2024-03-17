f573e93637bf0f6881d1ecdee0410b7f462fd0ae
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-17 17:46:17 +0530

    trimStart or trimEnd is only stored and used, means we can't mutate the strings with trim

--- practice.js
+++ practice.js
@@ -50,12 +50,14 @@ const pokemon = {
         age: 4,
         weight:25,
         height:12,
-        power:"Lightning"
+        power:"              Lightning"
     }
 }
-let power = pokemon?.pikachu?.power || "No Power"
+const sample = pokemon.pikachu.power.trimStart(14);
+let power = pokemon?.pikachu?.power ?? "No Power"
 console.log(power)
 
+console.log(sample)
 
 
 
